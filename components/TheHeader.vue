e55c7816d125e99de490e21d392ce56271a57063
TakKokubu <kokubu.tk@gmail.com>
2022-02-18 14:30:28 +0900

    headerの作成

---
+++ components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header-list">
+    <ul class="memo-header">
+      <li class="header-items"><nuxt-link to="/memo">メモ一覧</nuxt-link></li>
+      <li class="header-items">
+        <nuxt-link to="/newmemo">メモ作成</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+
+<style scoped>
+.header-list {
+  height: 40px;
+}
+.header-items {
+  color: white;
+  display: inline-block;
+  padding: 10px;
+  text-decoration: none;
+}
+</style>
