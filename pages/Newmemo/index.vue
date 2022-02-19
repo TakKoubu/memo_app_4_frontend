28143a5ddb957b704d707a60bc5af6196d1aa205
TakKoubu <48621700+TakKoubu@users.noreply.github.com>
2022-02-19 21:18:42 +0900

    create-memo (#3)

---
+++ pages/Newmemo/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-form">
+    <textarea class="memo-text" v-model="content"></textarea>
+    <button class="memo-btn" @click="addMemo">作成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+    };
+  },
+  methods: {
+    addMemo() {
+      this.$store.dispatch("addMemo", this.content).then(() => {
+        this.$router.push("/memo");
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.text-form {
+  margin-left: 45px;
+}
+.memo.text {
+  display: block;
+  margin-left: 10px;
+}
+.memo-btn {
+  display: block;
+}
+</style>
